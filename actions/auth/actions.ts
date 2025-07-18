9c807ed67ce6d4431d7d1aa443371419bd3d270b
Bassem-Adel-Eg <bassema@silverkeytech.com>
2025-07-18 16:06:57 +0300

    fix name in register

--- actions/auth/actions.ts
+++ actions/auth/actions.ts
@@ -12,9 +12,12 @@ export async function signup(formData: FormData) {
   const data = {
     email: formData.get("email") as string,
     password: formData.get("password") as string,
-    data: { 'full_name': formData.get("name") as string }
+    options: {
+      data: {
+        full_name: formData.get("name") as string,
+      },
+    }
   };
-
   const { error } = await supabase.auth.signUp(data);
 
   if (error) {
